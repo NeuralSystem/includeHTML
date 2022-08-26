c45a702b9576a760f9001e1c754a645c6cbfca93
admin <admin@eq19.com>
2022-08-26 13:57:00 +0700

    Update include.js

--- docs/js/include.js
+++ docs/js/include.js
@@ -8,23 +8,23 @@ window.include = function(url){
 
 
 /* core.JS
- ========================================================*/
+ ========================================================
 include('includeHTML/js/includeHTML.js');
 include('slick/slick/slick.min.js');
 include('sequence/js/svginnerhtml.min.js');
 include('sequence/js/snap.svg-min.js');
 include('flowchart/raphael-min.js');
 include('route/d3.min.js');
-include('cards/widget.js');
+include('cards/widget.js');*/
 
 /* library
- ========================================================*/
+ ========================================================
 include('js/device.min.js');
 include('js/jquery.cookie.js');
 include('js/jquery.ui.totop.js');
 include('js/jquery.easing.1.3.js');
 include('js/jquery.equalheights.js');
-include('js/jquery-migrate-1.2.1.js');
+include('js/jquery-migrate-1.2.1.js');*/
 
 
 /* Smooth scrolling
@@ -33,14 +33,19 @@ include('wheel/jquery.mousewheel.min.js');
 include('js/jquery.simplr.smoothscroll.min.js');*/
 
 /* Superfish menu
- ========================================================*/
+ ========================================================
 include('js/superfish.js');
-include('js/jquery.mobilemenu.js');
+include('js/jquery.mobilemenu.js');*/
 
 /* Custom script
- ========================================================*/
+ ========================================================
 include('js/legacy.js');
 include('js/camera.js');
 include('js/isotope.min.js');
 include('js/owl.carousel.js');
-include('js/function.js');
+include('js/function.js');*/
+
+include('js/jquery.min.js');
+include('js/bootstrap.min.js');
+include('js/stickUp.min.js');
+include('js/colorbox/jquery.colorbox-min.js');
