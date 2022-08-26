d8b4d4183d3e0b73e785bdefdbebb17201535193
admin <admin@eq19.com>
2022-08-26 16:09:57 +0700

    Update include.js

--- docs/js/include.js
+++ docs/js/include.js
@@ -8,8 +8,8 @@ window.include = function(url){
 
 /* core.JS
  ========================================================
-include('js/jquery-1.11.0.js');*/
-include('js/bootstrap-3.1.1.js');
+include('js/jquery-1.11.0.js');
+include('js/bootstrap-3.1.1.js');*/
 include('js/stickUp.min.js');
 include('js/colorbox/jquery.colorbox-min.js');
  
