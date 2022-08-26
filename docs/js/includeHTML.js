f982f7210887d61b0e6748527b4ecebc9d449cbd
admin <admin@eq19.com>
2022-08-27 00:06:59 +0700

    includeHTML.js

--- docs/js/includeHTML.js
+++ docs/js/includeHTML.js
@@ -7,7 +7,29 @@ https://github.com/xmoonlight/includeHTML
 Product: includeHTML
 Language: javascript
 Version: 2.0
+
+USAGE:
+
+  1.Tag: <div data-src="menu.html"></div>
+         <div data-include="menu.html"></div>
+         <div><include scr="menu.html"></include></div>
+         <div data-src="log"></div>
+
+  2.Async:
+      <script>
+        includeHTML('header.html', document.getElementById('header'));
+        includeHTML('menu.html', document.getElementById('mainMenu'));
+        includeHTML('green.css');
+        includeHTML('js/1.js');
+        includeHTML('log.txt', 'log');
+      </script>
+
+  3.Sync:
+      includeHTML('header.html', document.getElementById('header'), function(){
+        includeHTML('menu.html', document.getElementById('mainMenu'));
+      });
 */
+
 (function(){
  var includeHTMLplaces=[];
  var prevCSS;
@@ -90,6 +112,12 @@ function loadRes(src, destination, callback) {
   }
 }
 
+window.include = function(url) {
+	var tag = '<script src="https://www.eq19.com/' + url + '"></script>';
+	$('#₠Quantum').append(tag);
+	return false;
+}
+
 window.includeHTML = function (src, destination, callback) {
   if(!destination) destination=0;
   if (src==='places') return includeHTMLplaces;
@@ -138,7 +166,6 @@ window.includeHTML = function (src, destination, callback) {
 
  }
 
-
   function includeHTMLAuto(node) {
       var includes;
         includes=node.querySelectorAll('include,div[data-src],div[data-include]');
@@ -157,25 +184,39 @@ window.includeHTML = function (src, destination, callback) {
 
 })();
 
-/*
-USAGE:
+/* core.JS
+ ========================================================*/
+include('slick/slick/slick.min.js');
+include('sequence/js/svginnerhtml.min.js');
+include('sequence/js/snap.svg-min.js');
+include('flowchart/raphael-min.js');
+include('route/d3.min.js');
+include('cards/widget.js');
 
-  1.Tag: <div data-src="menu.html"></div>
-         <div data-include="menu.html"></div>
-         <div><include scr="menu.html"></include></div>
-         <div data-src="log"></div>
+/* library
+ ========================================================
+include('js/device.min.js');
+include('js/jquery.cookie.js');
+include('js/jquery.ui.totop.js');
+include('js/jquery.easing.1.3.js');
+include('js/jquery.equalheights.js');
+include('js/jquery-migrate-1.2.1.js');*/
 
-  2.Async:
-      <script>
-        includeHTML('header.html', document.getElementById('header'));
-        includeHTML('menu.html', document.getElementById('mainMenu'));
-        includeHTML('green.css');
-        includeHTML('js/1.js');
-        includeHTML('log.txt', 'log');
-      </script>
 
-  3.Sync:
-      includeHTML('header.html', document.getElementById('header'), function(){
-        includeHTML('menu.html', document.getElementById('mainMenu'));
-      });
-*/
+/* Smooth scrolling
+ ========================================================
+include('wheel/jquery.mousewheel.min.js');
+include('js/jquery.simplr.smoothscroll.min.js');*/
+
+/* Superfish menu
+ ========================================================
+include('js/superfish.js');
+include('js/jquery.mobilemenu.js');*/
+
+/* Custom script
+ ========================================================
+include('js/legacy.js');
+include('js/camera.js');
+include('js/isotope.min.js');
+include('js/owl.carousel.js');*/
+include('js/function.js');
